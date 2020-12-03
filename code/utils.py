b99f62ca6704c080d33eb6a9e803dbf2edd96bdf
esilkas <kasper.silverforsen@ericsson.com>
2020-12-03 12:16:59 +0100

    Added function to utils.py that takes the mean of the feature values per band

--- code/utils.py
+++ code/utils.py
@@ -3,3 +3,7 @@
 #Function which sums up the number of vehicles per StartTime.
 def groupby_stationid(df):
     return df[['StartTime','NumberOfVehicles']].groupby('StartTime').sum().reset_index()
+
+#Function which takes the mean of each band per timestamp
+def groupby_band(df):
+    return df[['StartTime']+[x for x in df.columns if 'PL' in x]].groupby('StartTime').mean().reset_index()
