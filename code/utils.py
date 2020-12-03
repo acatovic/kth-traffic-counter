2e4b4e054688c89226a165ee1d5bc4a8bcee07f9
esilkas <kasper.silverforsen@ericsson.com>
2020-12-03 15:39:14 +0100

    Added function that pivots data, if we don't want to merge features

--- code/utils.py
+++ code/utils.py
@@ -5,5 +5,13 @@ def groupby_stationid(df):
     return df[['StartTime','NumberOfVehicles']].groupby('StartTime').sum().reset_index()
 
 #Function which takes the mean of each band per timestamp
-def groupby_band(df):
-    return df[['StartTime']+[x for x in df.columns if 'PL' in x]].groupby('StartTime').mean().reset_index()
+def groupby_band(df,mode):
+    df = df.copy()
+    if mode == 'aggregate_mean':
+        return df[['StartTime']+[x for x in df.columns if 'PL' in x]].groupby('StartTime').mean().reset_index()
+    elif mode =='pivot_band':
+        pivot_vals = [x for x in df.columns if 'PL_' in x]
+        df = df.pivot_table(values=pivot_vals, index='StartTime', columns='Band',aggfunc='mean').reset_index()
+        df.columns = df.columns.map('{0[0]}|{0[1]}'.format)
+        df = df.rename(columns={'StartTime|':'StartTime'})
+        return df
