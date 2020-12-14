37f68442c4b8ebac46e5f1fb8349f5dc2cce0abe
acatovic <armin.catovic@hotmail.se>
2020-12-14 21:57:23 +0100

    Updated utils.py and created the main project notebook with combined snippets from armins and kaspers work; cleaned up and simplified the code and models; still need to add models with time window (96) features, and LSTM

--- code/utils.py
+++ code/utils.py
@@ -1,5 +1,4 @@
-# Python Script which contains useful help functions for making transformations to target and feature dataset
-
+import numpy as np
 import pandas as pd
 
 def groupby_stationid(df):
@@ -7,9 +6,9 @@ def groupby_stationid(df):
     
     Assume `df` has the following columns:
     
-      o StartTime: Starting date + time of 15-min sample interval
-      o StationId: Id (int64) of traffic sensor/station
-      o NumberOfVehicles: 15-min aggregate vehicle count
+        o StartTime: Starting date + time of 15-min sample interval
+        o StationId: Id (int64) of traffic sensor/station
+        o NumberOfVehicles: 15-min aggregate vehicle count
     '''
     stations = df['StationId'].unique()
     df['StartTime'] = pd.to_datetime(df['StartTime'])
@@ -25,13 +24,57 @@ def groupby_stationid(df):
     
 
 #Function which takes the mean of each band per timestamp
-def groupby_band(df,mode):
-    df = df.copy()
-    if mode == 'aggregate_mean':
-        return df[['StartTime']+[x for x in df.columns if 'PL' in x]].groupby('StartTime').mean().reset_index()
-    elif mode =='pivot_band':
-        pivot_vals = [x for x in df.columns if 'PL_' in x]
-        df = df.pivot_table(values=pivot_vals, index='StartTime', columns='Band',aggfunc='mean').reset_index()
-        df.columns = df.columns.map('{0[0]}|{0[1]}'.format)
-        df = df.rename(columns={'StartTime|':'StartTime'})
-        return df
+#def groupby_band(df,mode):
+#    df = df.copy()
+#    if mode == 'aggregate_mean':
+#        return df[['StartTime']+[x for x in df.columns if 'PL' in x]].groupby('StartTime').mean().reset_index()
+#    elif mode =='pivot_band':
+#        pivot_vals = [x for x in df.columns if 'PL_' in x]
+#        df = df.pivot_table(values=pivot_vals, index='StartTime', columns='Band',aggfunc='mean').reset_index()
+#        df.columns = df.columns.map('{0[0]}|{0[1]}'.format)
+#        df = df.rename(columns={'StartTime|':'StartTime'})
+#        return df
+
+def groupby_band(df):
+    '''Group by band and sum up path loss values of cells on the same band.
+    
+    Assume `df` has the following columns:
+    
+      o StartTime: Starting date/time of 15-min sample interval, (obj)
+      o Band: Carrier frequency in MHz of each cell, (int64)
+      o CellId: Local cell id in LTE eNB, (int64)
+      o PL_00 to PL_20: Path loss values in dB bins, (float64)
+    '''
+    df['StartTime'] = pd.to_datetime(df['StartTime'])
+    values = df.columns[3:]
+    df = df.pivot_table(values=values, index='StartTime', columns='Band', 
+                        aggfunc='sum').reset_index()
+    df.columns = df.columns.map('{0[0]}|{0[1]}'.format)
+    df = df.rename(columns={'StartTime|': 'StartTime'})
+    
+    return df
+
+def ts_train_test_split(X, y, train_range, test_range):
+  '''Time-series train/test split, where train/test data is specified by 
+  tuples consisting of start/end datetime values.
+  '''
+  X_train = X.loc[train_range[0]: train_range[1]]
+  y_train = y.loc[train_range[0]: train_range[1]]
+  X_test = X.loc[test_range[0]: test_range[1]]
+  y_test = y.loc[test_range[0]: test_range[1]]
+  return X_train, X_test, y_train, y_test
+
+
+def cost_hata_distance(L, f, hB, hR=1.8, Cm=0.):
+    '''Calculate distance between RBS and user, assuming COST Hata RF 
+    propagation model, https://en.wikipedia.org/wiki/COST_Hata_model.
+    
+      o L:  Median path loss in dB
+      o f:  Carrier frequency/band in MHz
+      o hB: RBS antenna height in meters
+      o hR: Mobile station antenna height in meters
+      o Cm: Constant offset in dB; 0. for suburban and 3. for metropolitan
+            areas
+    '''
+    d = 1000 * 10 ** ((L - 46.3 - 33.9 * np.log10(f) + 13.82 * np.log10(hB) + (3.2 * (np.log10(11.75 * hR)) ** 2 - 4.97) - Cm) / (44.9 - 6.55 * np.log10(hB)))
+    return d
